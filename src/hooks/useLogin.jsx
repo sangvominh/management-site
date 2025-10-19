7090841d52a2b550e712c7752d78b049f869cf7c
Minh Sang <vosang20102005@gmail.com>
2025-10-19 23:36:24 +0700

    feat: login functionality with user online status update

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { projectAuth } from "../firebase/config";
+import { projectAuth, projectFirestore } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogin = () => {
@@ -14,6 +14,7 @@ export const useLogin = () => {
 
     try {
       const res = await projectAuth.signInWithEmailAndPassword(email, password);
+      await projectFirestore.collection("users").doc(res.user.uid).update({ online: true });
 
       // update context
       dispatch({ type: "LOGIN", payload: res.user });
