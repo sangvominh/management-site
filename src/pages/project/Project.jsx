390983ad3f9860b147b2414b27caba9221d794ff
Minh Sang <vosang20102005@gmail.com>
2025-10-22 23:10:38 +0700

    Implement project details page with summary and comments

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,10 +1,26 @@
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
 // styles
-import './Project.css'
+import "./Project.css";
+import ProjectSummary from "./ProjectSummary";
+import ProjectComment from "./ProjectComment";
 
 export default function Project() {
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id);
+
+  if(error) {
+    return <div className="error">{error}</div>
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>
+  }
+
   return (
-    <div>
-      Project details
+    <div className="project-details">
+      <ProjectSummary project={document} />
+      <ProjectComment project={document} />
     </div>
-  )
+  );
 }
