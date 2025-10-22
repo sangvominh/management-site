ad5da9323607203a548cd34c30e619500383b863
Minh Sang <vosang20102005@gmail.com>
2025-10-22 23:10:36 +0700

    Implement create project page with project submission and validation

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import Select from "react-select";
 import { useCollection } from "../../hooks/useCollection";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { timestamp } from "../../firebase/config";
+import { useFirestore } from "../../hooks/useFirestore";
 
 // styles
 import "./Create.css";
+import { useNavigate } from "react-router-dom";
 
 const categories = [
   { value: "development", label: "Development" },
@@ -13,20 +17,61 @@ const categories = [
 ];
 
 export default function Create() {
+  const navigate = useNavigate();
+  const { addDocument, response } = useFirestore("projects");
+  const { user } = useAuthContext();
+  // assignedUser
   const { documents } = useCollection("users");
   const [users, setUsers] = useState();
-
   // form field values
   const [name, setName] = useState("");
   const [details, setDetails] = useState("");
   const [dueDate, setDueDate] = useState("");
   const [category, setCategory] = useState("");
   const [assignedUsers, setAssignedUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setFormError(null);
+    if (!category) {
+      setFormError("Please fill category");
+      return;
+    }
+    if (assignedUsers.length < 1) {
+      setFormError("Please fill assignedUsers");
+      return;
+    }
 
-    console.log(name, details, dueDate, category.value, assignedUsers.value);
+    const createBy = {
+      id: user.uid,
+      displayName: user.displayName,
+    };
+
+    const assignedUsersList = assignedUsers.map((u) => {
+      return {
+        id: u.value.id,
+        displayName: u.value.displayName,
+      };
+    });
+
+    const project = {
+      name,
+      details,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      category: category.value,
+      assignedUsersList,
+      createBy,
+      comment: [],
+    };
+
+    // add document
+    await addDocument(project);
+
+    // redirect
+    if (!response.error) {
+      navigate("/");
+    }
   };
 
   useEffect(() => {
@@ -62,8 +107,10 @@ export default function Create() {
           <span>Assign to:</span>
           <Select onChange={(option) => setAssignedUsers(option)} options={users} isMulti />
         </label>
-
-        <button className="btn">Add Project</button>
+        {response.isPending && <button className="btn">Add Project ...</button>}{" "}
+        {!response.isPending && <button className="btn">Add Project</button>}{" "}
+        {response.error && <p className="error">{formError}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
